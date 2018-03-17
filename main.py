cc6ff8d1af85d5f70483d70b7f13763ce1629d63
sdlm <sdlmerr@yandex.ru>
2018-03-17 19:01:14 +0300

    style fixes

--- main.py
+++ main.py
@@ -1,103 +1,117 @@
 from itertools import groupby
 
+from pony.orm.core import Query
+
 from models import *
 
 
-def main():
+def get_biggest_new_category(queryset: Query):
+    """
+    Найдём самую большую категорию в размеченных данных
+    """
+
+    # названия категорий в размеченных данных
+    p2_cats = [x.eshop_category_name for x in queryset]
+    # сгруппируем по названию категорий
+    groups_of_cats = [(name, len(list(gr))) for name, gr in groupby(sorted(p2_cats))]
+    # выберем категорию которой размечено наибольшее кол-во товаров
+    cat_name, cat_size = sorted(groups_of_cats, key=lambda x: x[1], reverse=True)[0]
+
+    cat_id = None
+    for product in queryset:
+        if product.eshop_category_name == cat_name:
+            cat_id = product.eshop_category_id
+            break
+
+    return cat_id, cat_name, cat_size
+
+
+def mark_up():
+    """
+    Разметим данные используя группировку по категориям.
+    """
+
     with db_session:
         p1_count = Products_1.select().count()
         origin_p2_count = total_p2_count = Products_2.select().count()
-        p2_proc_in_p1 = int(total_p2_count / p1_count * 100)
+        p2_percent_in_p1 = int(total_p2_count / p1_count * 100)
 
         # Сначала пройдёмся по листам дерева категорий
         for cat in select(c for c in Categories if count(c.child_set) == 0):
 
             # продукты в рамках текущей категории
             p1 = list(cat.product_set)
-            if len(p1) > 0:
+            p1_len = len(p1)
+            if p1_len > 0:
 
                 # размеченные продукты в рамках текущей категории
                 p1_ids = [x.plu for x in p1]
-                p2 = list(select(p for p in Products_2 if p.plu in p1_ids))
-                if len(p2) > 0:
-                    proc = len(p2) / len(p1) * 100
+                p2 = select(p for p in Products_2 if p.plu in p1_ids)
+                p2_len = len(p2)
+                if p2_len > 0:
+                    marked_up_percent = p2_len / p1_len * 100
 
                     # если в текущей группе размечено менее 3% продуктов,
                     # то мы считаем что данных для разметки этой группы недостаточно
-                    if proc < 3:
+                    if marked_up_percent < 3:
                         continue
 
-                    # названия категорий в размеченных данных
-                    p2_cats = [x.eshop_category_name for x in p2]
-                    # сгруппируем по названию категорий
-                    groups_of_cats = [(name, len(list(gr))) for name, gr in groupby(sorted(p2_cats))]
-                    # выберем категорию которой размечено наибольшее кол-во товаров
-                    biggest_group_name, biggest_group_size = sorted(groups_of_cats, key=lambda x: x[0], reverse=True)[0]
+                    new_cat_id, new_cat_name, new_cat_size = get_biggest_new_category(p2)
 
                     # доля которая соответствует наибольшей категории среди размеченных данных
-                    cat2_proc = biggest_group_size / len(p2) * 100
+                    new_cat_percent = new_cat_size / p2_len * 100
 
                     # если категория с наибольшим кол-вом продуктов имеет долю менее половины
                     # то мы считаем что данных для разметки этой группы недостаточно
                     # т.е. в данной категории размеченные данные слишком разнородны
-                    if cat2_proc < 50:
+                    if new_cat_percent < 50:
                         continue
 
                     # дебаг
                     print(
                         '{:70s} {:5d} {:5d} {:>8.1f} {:>8.1f}   {}'.format(
-                            cat.name, len(p1), len(p2),
-                            proc, cat2_proc, biggest_group_name
+                            cat.name, p1_len, p2_len,
+                            marked_up_percent, new_cat_percent, new_cat_name
                         )
                     )
-                    addition = len(p1) - len(p2)
 
                     # нам важны как однородность размеченных данных так и процент размеченных данных
-                    # грубо, можно было бы сделать if cat2_proc + proc > 100,
+                    # грубо, можно было бы сделать if new_cat_percent + marked_up_percent > 100,
                     # но на мой взгляд это смотрится черезчур дико
                     mark_up_data = False
-                    if cat2_proc == 100:
+                    if new_cat_percent == 100:
                         mark_up_data = True
-                    elif cat2_proc > 90:
-                        if proc >= 10:
+                    elif new_cat_percent > 90:
+                        if marked_up_percent >= 10:
                             mark_up_data = True
-                    elif cat2_proc > 80:
-                        if proc >= 20:
+                    elif new_cat_percent > 80:
+                        if marked_up_percent >= 20:
                             mark_up_data = True
-                    elif cat2_proc > 70:
-                        if proc >= 30:
+                    elif new_cat_percent > 70:
+                        if marked_up_percent >= 30:
                             mark_up_data = True
 
                     # размечаем данные
                     if mark_up_data:
                         # определяем biggest_group_id
-                        biggest_group_id = None
-                        for product in p2:
-                            if product.eshop_category_name == biggest_group_name:
-                                biggest_group_id = product.eshop_category_id
-                                break
                         existed_p2_ids = [x.plu for x in p2]
 
                         # собственно дополняем таблицу Products_2
-                        fact_addition = 0
                         for p in select(p for p in Products_1 if p.plu in p1_ids and p.plu not in existed_p2_ids):
                             Products_2(
                                 plu=p.plu,
                                 title=p.title,
-                                eshop_category_id=biggest_group_id,
-                                eshop_category_name=biggest_group_name,
+                                eshop_category_id=new_cat_id,
+                                eshop_category_name=new_cat_name,
                             )
-                            fact_addition += 1
-                        if fact_addition != addition:
-                            print('wtf ' + '!'*50)
-                        total_p2_count += addition
+                        total_p2_count += p1_len - p2_len
 
                     # saves objects created by this moment in the database
                     flush()
 
-        print(f'total count of t1 products: {p1_count}')
-        print(f'total count of t2 products: {origin_p2_count}')
-        print(f'initial ratio: {p2_proc_in_p1}%')
+        print(f'total count of t1 products: {p1_count:6d}')
+        print(f'total count of t2 products: {origin_p2_count:6d}')
+        print(f'initial ratio: {p2_percent_in_p1}%')
         print(f'  final ratio: {int(total_p2_count / p1_count * 100)}%')
 
         rollback()
---
+++ mark_up_products.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from main import mark_up
+
+if __name__ == "__main__":
+    mark_up()
